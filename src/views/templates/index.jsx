ca3fd16f8048483fcc9697dc4af270015dd60696
weigaosS <weigaosS@users.noreply.github.com>
2022-01-12 10:03:07 +0800

    add scss for css

--- src/views/templates/index.jsx
+++ src/views/templates/index.jsx
@@ -1,7 +1,8 @@
 import Flex from '@cobalt/react-flex';
+import styles from './index.module.scss';
 
 const Templates = () => {
-  return <Flex>this is Templates component!</Flex>;
+  return <Flex className={styles.title}>this is Templates component!</Flex>;
 };
 
 export default Templates;
